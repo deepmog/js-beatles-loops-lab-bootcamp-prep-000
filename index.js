ccc4a02ca2d03281b24f566fcbfcea6589d8cec3
epic-constructor-9765 <barton.e.marks@gmail.com>
2019-08-13 15:40:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments) {
+ var hollowray = [];
+  for (var i = 0; i < 4; i++)
+  {
+    if (i === 0) { 
+        var stringx = `${musicians[0]} plays ${instruments[0]}`;
+        hollowray.push(stringx); 
+   } else {
+        var stringy = `${musicians[i]} plays ${instruments[i]}`;
+        hollowray.push(stringy);
+    }
+  }
+  return hollowray;
+}
+
+
+   var crzyboutlennon = [];
+function johnLennonFacts(facts) {
+ 
+    var n = 0;
+    while (n < facts.length) {
+    var string = `${facts[n]}!!!`;
+   crzyboutlennon.push(string);
+    n++;
+    
+  }return crzyboutlennon;
+}
+
